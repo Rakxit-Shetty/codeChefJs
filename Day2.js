7504f48b58760b5d11191a130bb65fe41dd9cd8f
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-01 20:24:31 +0530

    day2-03

--- Day2.js
+++ Day2.js
@@ -34,3 +34,38 @@ rl02.on('close',()=>{
     let res=line.map((ele)=>ele.split(" "));
     res.forEach((ele)=>console.log(Number(ele[0])-Number(ele[1])))
 })
+
+//Wordle
+
+const readline=require('readline');
+const rl03=readline.createInterface(process.stdin,process.stdout);
+
+let line3=[];
+let st=[];
+let count=-1;
+
+rl03.on('line',(read)=>{
+   count++;
+    if(count>0){
+    
+    st.push(read);
+    if(count%2==0){
+        line3.push(st);
+        st=[];
+    }
+    }
+})
+
+rl03.on('close',()=>{
+    
+    let res=line3.map((str)=>{
+         let fin="";
+        for(let i=0;i<str[0].length;i++){
+           
+            str[0][i]==str[1][i] ? fin+="G" :fin+="B"
+        }
+        return fin;
+    })
+    
+    res.forEach((ele)=>console.log(ele));
+})
