96a08385d4e4325a00c6d00b31486cca4714c445
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-01 20:01:33 +0530

    day2-02

--- Day2.js
+++ Day2.js
@@ -15,3 +15,22 @@ rl01.on('close',()=>{
   res.forEach((ele)=>console.log(ele))
    
 })
+
+
+//Saving Taxes
+
+const readline=require('readline');
+const rl02=readline.createInterface(process.stdin,process.stdout);
+
+let line1=[]
+
+rl02.on('line',(read)=>{
+    line1.push(read);
+})
+
+rl02.on('close',()=>{
+    line.splice(0,1);
+    
+    let res=line.map((ele)=>ele.split(" "));
+    res.forEach((ele)=>console.log(Number(ele[0])-Number(ele[1])))
+})
