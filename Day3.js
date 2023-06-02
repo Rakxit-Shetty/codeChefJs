4abb44852f04302e21e55f95122243c2ab7b2596
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-02 18:54:18 +0530

    Day3-02

--- Day3.js
+++ Day3.js
@@ -17,3 +17,29 @@ rl.on('line',(read)=>{
 rl.on('close',()=>{
     line.forEach((ele)=>console.log(Number(ele[0])*Number(ele[1])))
 })
+
+//The Block Game
+
+const readline=require('readline');
+const rl=readline.createInterface(process.stdin,process.stdout);
+
+let skip1=-1;
+let arr=[]
+
+rl.on('line',(read)=>{
+    skip1++;
+    if(skip1>0){
+        arr.push(read.split(""));
+    }
+})
+
+rl.on('close',()=>{
+    
+    arr.forEach((ele)=> {
+        
+        ele.join("")===ele.reverse().join("") ?
+     console.log("wins") : console.log("loses") 
+        
+    })
+    
+})
