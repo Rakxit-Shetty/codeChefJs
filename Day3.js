40a012025c7076ed956c96408d64932a2a82dc1c
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-03 23:19:48 +0530

    Day4-Atm

--- Day3.js
+++ Day3.js
@@ -18,66 +18,66 @@ rl.on('close',()=>{
     line.forEach((ele)=>console.log(Number(ele[0])*Number(ele[1])))
 })
 
-//The Block Game
+// //The Block Game
 
-const readline=require('readline');
-const rl=readline.createInterface(process.stdin,process.stdout);
+// const readline=require('readline');
+// const rl=readline.createInterface(process.stdin,process.stdout);
 
-let skip1=-1;
-let arr=[]
+// let skip1=-1;
+// let arr=[]
 
-rl.on('line',(read)=>{
-    skip1++;
-    if(skip1>0){
-        arr.push(read.split(""));
-    }
-})
+// rl.on('line',(read)=>{
+//     skip1++;
+//     if(skip1>0){
+//         arr.push(read.split(""));
+//     }
+// })
 
-rl.on('close',()=>{
+// rl.on('close',()=>{
     
-    arr.forEach((ele)=> {
+//     arr.forEach((ele)=> {
         
-        ele.join("")===ele.reverse().join("") ?
-     console.log("wins") : console.log("loses") 
+//         ele.join("")===ele.reverse().join("") ?
+//      console.log("wins") : console.log("loses") 
         
-    })
+//     })
     
-})
+// })
 
-// read 3 input by skiping 1
-//Uncle Johny
+// // read 3 input by skiping 1
+// //Uncle Johny
 
 
-const readline=require('readline');
-const rl=readline.createInterface(process.stdin,process.stdout);
+// const readline=require('readline');
+// const rl=readline.createInterface(process.stdin,process.stdout);
 
-let skip=-1;
-let ar=[];
-let line=[];
-let thr=0;
+// let skip=-1;
+// let ar=[];
+// let line=[];
+// let thr=0;
 
-rl.on('line',(read)=>{
-    skip++;
+// rl.on('line',(read)=>{
+//     skip++;
     
-    if(skip>0){
+//     if(skip>0){
         
-        ar.push(read)
+//         ar.push(read)
         
-        if(thr===2){
-          line.push(ar)
-          ar=[];
-          thr=-1;
-        }
+//         if(thr===2){
+//           line.push(ar)
+//           ar=[];
+//           thr=-1;
+//         }
         
-        thr++;
-        }
+//         thr++;
+//         }
         
     
-})
+// })
 
-rl.on('close',()=>{
-    // console.log(line);
-    line.forEach((ele)=>console.log([...ele[1]].filter((e)=>e!=" ").at(Number(ele[2])-1))) 
-})
+// rl.on('close',()=>{
+//     // console.log(line);
+//     line.forEach((ele)=>console.log([...ele[1]].filter((e)=>e!=" ").at(Number(ele[2])-1))) 
+// })
 
 
--- Day4.js
+++ Day4.js
@@ -18,53 +18,53 @@ rl.on('close',()=>{
     res.forEach((ele)=>ele==6 ? console.log("YES"): console.log("NO"))
 })
 
-//Easy Pronunciation
-const readline=require('readline');
+// //Easy Pronunciation
+// const readline=require('readline');
 
-const rl=readline.createInterface(process.stdin,process.stdout);
+// const rl=readline.createInterface(process.stdin,process.stdout);
 
-let nlythree=[];
-let all=[];
-let skip=-1;
-let thr=0;
+// let nlythree=[];
+// let all=[];
+// let skip=-1;
+// let thr=0;
 
-rl.on('line',(read)=>{
-    skip++;
-    if(skip>0){
-        nlythree.push(read);
-        thr++;
-        if(thr===2){
-            all.push(nlythree)
-            nlythree=[]
-            thr=0;
-        }
-    }
-})
+// rl.on('line',(read)=>{
+//     skip++;
+//     if(skip>0){
+//         nlythree.push(read);
+//         thr++;
+//         if(thr===2){
+//             all.push(nlythree)
+//             nlythree=[]
+//             thr=0;
+//         }
+//     }
+// })
 
-rl.on('close',()=>{
+// rl.on('close',()=>{
     
-    all.forEach((ele)=>{
+//     all.forEach((ele)=>{
         
-        if(Number(ele[0])<4){
+//         if(Number(ele[0])<4){
             
-            console.log("YES");
+//             console.log("YES");
             
-        }else{
+//         }else{
             
-            let count=0;
+//             let count=0;
             
-            for(elem of [...ele[1]]){
+//             for(elem of [...ele[1]]){
                 
-                if(elem!='a' && elem!='e' && elem!='i' && elem!='o' && elem!='u'){
-                count++;
-              if(count>=4) break;
-                }else{
-                    count=0;
-                }
-            }
-             count>=4 ? console.log("NO"): console.log("YES") 
+//                 if(elem!='a' && elem!='e' && elem!='i' && elem!='o' && elem!='u'){
+//                 count++;
+//               if(count>=4) break;
+//                 }else{
+//                     count=0;
+//                 }
+//             }
+//              count>=4 ? console.log("NO"): console.log("YES") 
             
-        }
-    })
+//         }
+//     })
     
-})
+// })
